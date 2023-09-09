ecbd746a996ff702da71a10753d135e61d262f60
Apsy_Prakash <apsyajeesh@gmail.com>
2023-09-09 00:04:00 -0700

    page factory

--- src/test/java/page/CartPage.java
+++ src/test/java/page/CartPage.java
@@ -1,34 +1,43 @@
 package page;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class CartPage {
-    By addToCartButton = By.cssSelector("img[alt='Cart']");
-    By proceedToCheckoutButton = By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]");
-    By promoCodeTextBox = By.cssSelector("input[class='promoCode']");
-    By promoCodeSubmitButton = By.cssSelector("button[class='promoBtn']");
+
+    @FindBy(css = "img[alt='Cart']")
+    WebElement addToCartButton;
+    @FindBy(xpath = "//button[contains(text(),'PROCEED TO CHECKOUT')]")
+    WebElement proceedToCheckoutButton;
+    @FindBy(css = "input[class='promoCode']")
+    WebElement promoCodeTextBox;
+    @FindBy(css = "button[class='promoBtn']")
+    WebElement promoCodeSubmitButton;
 
     WebDriver driver;
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(this.driver, this);
     }
 
     public void applyPromoCode() {
         System.out.println("Applying promo code");
-        driver.findElement(addToCartButton).click();
-        driver.findElement(proceedToCheckoutButton).click();
-        driver.findElement(promoCodeTextBox).sendKeys("rahulshettyacademy");
-        driver.findElement(promoCodeSubmitButton).click();
+        addToCartButton.click();
+        proceedToCheckoutButton.click();
+        promoCodeTextBox.sendKeys("rahulshettyacademy");
+        promoCodeSubmitButton.click();
         Duration timeout = Duration.ofSeconds(5);
         WebDriverWait webDriverWait = new WebDriverWait(driver, timeout);
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(promoCodeSubmitButton));
+        webDriverWait.until(ExpectedConditions.visibilityOf(promoCodeSubmitButton));
         System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText() + " promo code applied succesfully");
     }
 
